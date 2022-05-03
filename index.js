dfbe7ce9d2c62afd124092ef6015214b3afc25fb
bryanjob <74436343+bryanjob@users.noreply.github.com>
2022-05-03 11:05:16 -0500

    MITXPRO-12: add code for assignment

--- index.js
+++ index.js
@@ -97,21 +97,45 @@ type Mutation {
 // The root provides a resolver function for each API endpoint
 
 var root = {
+  // added this to get next id
+  getId: () => {
+    return restaurants.reduce((a, b) => (a.id > b.id) ? a.id : b.id, 0) + 1;
+  }, 
+  // added this to get index of restaurant queried
+  getIndex: id => {
+    return restaurants.findIndex(restaurant => restaurant.id === id);
+  },
   restaurant: (arg) => {
     // Your code goes here
+    return restaurants[root.getIndex(arg.id)];
   },
   restaurants: () => {
     // Your code goes here
+    return restaurants;
   },
   setrestaurant: ({ input }) => {
     // Your code goes here
+    restaurants.push({id: root.getId(), name: input.name, description: input.description});
+    return input;
   },
   deleterestaurant: ({ id }) => {
     // Your code goes here
+    let indexIdToDelete = root.getIndex(id);
+    const ok = Boolean(indexIdToDelete > -1);
+    restaurants = restaurants.filter(restaurant => restaurant.id !== id);
+    return {ok};
   },
   editrestaurant: ({ id, ...restaurant }) => {
     // Your code goes here
-  },
+    let indexIdToEdit = root.getIndex(id);
+    if(indexIdToEdit < 0) {
+      throw new Error("restaurant does not exist");
+    }
+    restaurants[indexIdToEdit] = {
+    ...restaurants[indexIdToEdit], ...restaurant
+    }
+    return restaurants[indexIdToEdit];
+  }
 };
 var app = express();
 app.use(
@@ -125,4 +149,4 @@ app.use(
 var port = 5500;
 app.listen(5500, () => console.log("Running Graphql on Port:" + port));
 
-export default root;
+// export default root;
